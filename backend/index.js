abe9f99c092b6237f1c8f253a6c382848831be9c
snaipp1 <snaipp1@yandex.by>
2021-01-07 21:41:50 +0300

    feat: add handlebars

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const express_handlebars = require('express-handlebars');
+const path = require('path');
+
 const app = express();
+
+const hbs = express_handlebars.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 const PORT = process.env.PORT || 3000;
 
