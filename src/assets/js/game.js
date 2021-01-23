754417e73bd9d1c73300ad00a188d9528a61d548
Leonid Rusakov <bengunn87@mail.ru>
2021-01-23 16:50:50 +0500

    feat: add level, score, timer and buttons of training

---
+++ src/assets/js/game.js
@@ -0,0 +1,20 @@
+export const gameLevel = {
+  easy: {
+    name: 'легкий',
+    duration: 60,
+  },
+  medium: {
+    name: 'средний',
+    duration: 50,
+  },
+  hard: {
+    name: 'тяжелый',
+    duration: 45,
+  },
+};
+
+export const gameStatus = {
+  init: 'init',
+  start: 'start',
+  stop: 'stop',
+};
